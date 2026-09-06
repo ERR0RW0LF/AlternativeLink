89138219861a17e4fdd4679f539a8bc65dd39ba6
ERR0RW0LF <finn.woma@gmail.com>
2026-09-06 15:50:27 +0200

    added test cases for the `Code` struct and the `Message` enum.

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ struct SharedState {
     other_link_ip: watch::Sender<Option<Ipv4Addr>>,
 }
 
+#[derive(PartialEq, Debug)]
 enum Message {
     Discover(Code, Ipv4Addr),
     Ping(Ipv4Addr),
@@ -66,7 +67,44 @@ impl TryFrom<&str> for Message {
     
 }
 
-#[derive(PartialEq, Clone, Copy)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_code_to_string() {
+        assert_eq!(Code { code_uint: 1337 }.to_string() , "1-3-3-7")
+    }
+
+    #[test]
+    fn test_code_try_from() {
+        assert!(Code::try_from("1-3-3-7").is_ok());
+        assert!(Code::try_from("foo-bar").is_err())
+    }
+
+    #[test]
+    fn test_message_try_from_ping() {
+        assert_eq!(Message::try_from("PING 127.0.0.1"), Ok(Message::Ping(Ipv4Addr::new(127, 0, 0, 1))));
+        assert!(Message::try_from("PING 127.0.0.").is_err());
+        assert!(Message::try_from("PONG 127.0.0.1").is_err());
+    }
+
+    #[test]
+    fn test_message_try_from_ack() {
+        assert_eq!(Message::try_from("ACK 127.0.0.1"), Ok(Message::Ack(Ipv4Addr::new(127, 0, 0, 1))));
+        assert!(Message::try_from("ACK 127.0.0.").is_err());
+        assert!(Message::try_from("A 127.0.0.1").is_err());
+    }
+
+    #[test]
+    fn test_message_try_from_discover() {
+        assert_eq!(Message::try_from("1-3-3-7 127.0.0.1"), Ok(Message::Discover(Code {code_uint:1337}, Ipv4Addr::new(127, 0, 0, 1))));
+        assert!(Message::try_from("1-3-3-7 127.0.0.1").is_ok());
+        assert!(Message::try_from("1-3-3-b 127.0.0.1").is_err());
+    }
+}
+
+#[derive(PartialEq, Clone, Copy, Debug)]
 struct Code {
     code_uint: usize
 }
